3a5a12dacd9c92a30eed365bc5499415b292b81d
HToy91 <jfvargasfigueroa@email.neit.edu>
2025-03-06 13:53:37 -0500

    I think I'm done
    -Created the data folder with accounts.json file
    
    -Created the get for sign up page and /accounts which brings you to json file
    
    -Created post for adding a new account with boolean to check for existing accounts
    
    -Created get for login page
    
    -Created post for login page that sends you to account page if username and password match JSON file. used a boolean

---
+++ app.js
@@ -0,0 +1,95 @@
+// imports to set up the basic tools you'll need to build a web server with Express, handle file operations, and manage file paths.
+const express = require("express");
+const app = express();
+const fs = require("fs");
+const path = require("path");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+// app.get('/favicon.ico', (req, res) => res.status(204)); ---> is this needed?
+
+const readFile = (path) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, `utf8`, (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+};
+
+// Get to sign up page
+app.get(`/signup`, (req, res) => {
+  const filePath = path.join(__dirname, `public`, `signup.html`);
+  res.send(filePath);
+});
+// Get all accounts
+app.get(`/accounts`, async (req, res) => {
+  var data = await readFile(`./data/accounts.json`);
+  res.send(data);
+});
+// Add a new account
+app.post(`/accounts`, async (req, res) => {
+  var oldData = await readFile(`./data/accounts.json`);
+  var newData = await JSON.parse(oldData);
+
+  // use boolean to determine if new account is already in the list
+  var found = false;
+  for (var i = 0; i < newData.length; i++) {
+    if (newData[i].username === req.body.username) {
+      found = true;
+      console.log(`Account already exists`);
+      res.send(`Account already exists`);
+      return;
+    }
+  }
+
+  if (!found) {
+    newData.push(req.body);
+    const jsonString = JSON.stringify(newData);
+    await fs.writeFile(`./data/accounts.json`, jsonString, (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(`Account added`);
+      }
+    });
+    res.send(jsonString);
+  }
+});
+
+// Get to login page
+app.get(`/login`, (req, res) => {
+  const filePath = path.join(__dirname, `public`, `login.html`);
+  res.send(filePath);
+});
+
+// Get to account page if login info matches username and password in JSON file
+app.post(`/login`, async (req, res) => {
+  var oldData = await readFile(`./data/accounts.json`);
+  var newData = await JSON.parse(oldData);
+  var found = false;
+
+  for (var i = 0; i < newData.length; i++) {
+    if (newData[i].username === req.body.username && newData[i].password === req.body.password) {
+      found = true;
+      console.log(`Login successful`);
+      res.sendFile(path.join(__dirname, `public`, `accounts.html`));
+      return;
+    }
+  }
+
+  if (!found) {
+    console.log(`Login failed`);
+    res.send(`Login failed`);
+  }
+});
+
+//Start up the server on port 3000.
+var port = process.env.PORT || 80;
+app.listen(port, () => {
+  console.log("Server Running at Localhost:80");
+});
